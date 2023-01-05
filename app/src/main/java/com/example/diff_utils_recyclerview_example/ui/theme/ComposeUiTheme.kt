3c45797de27922676abbc844a1b04c7bcbd4c984
Njumbi <wambuinjumbi@gmail.com>
2023-01-05 15:54:23 +0300

    change status bar color

--- app/src/main/java/com/example/diff_utils_recyclerview_example/ui/theme/ComposeUiTheme.kt
+++ app/src/main/java/com/example/diff_utils_recyclerview_example/ui/theme/ComposeUiTheme.kt
@@ -39,7 +39,7 @@ fun ComposeUiTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            (view.context as Activity).window.statusBarColor = white.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
