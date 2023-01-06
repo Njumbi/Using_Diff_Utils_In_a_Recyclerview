318d707eff26fe2bf12451c74bf79ceb49c3b5ab
Njumbi <wambuinjumbi@gmail.com>
2023-01-06 18:21:23 +0300

    add view model

--- app/src/main/java/com/example/diff_utils_recyclerview_example/ui/viewmodel/FetchCharactersViewModel.kt
+++ app/src/main/java/com/example/diff_utils_recyclerview_example/ui/viewmodel/FetchCharactersViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.diff_utils_recyclerview_example.ui.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.diff_utils_recyclerview_example.data.remote.ResponseState
 import com.example.diff_utils_recyclerview_example.data.repository.CharactersRepo
-import com.example.diff_utils_recyclerview_example.data.responses.ApiResource
-import com.example.diff_utils_recyclerview_example.domain.models.CharacterModel
+import com.example.diff_utils_recyclerview_example.domain.models.CharacterDetails
+import com.example.diff_utils_recyclerview_example.ui.state.CharactersDetailsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,26 +18,38 @@ import javax.inject.Inject
 class FetchCharactersViewModel @Inject constructor(val charactersRepo: CharactersRepo) :
     ViewModel() {
 
-    private val _fetchCharacters = MutableLiveData<CharacterModel>()
-    val fetchCharacters: LiveData<CharacterModel> get() = _fetchCharacters
+    private val _charactersDetailsUiState = MutableStateFlow(CharactersDetailsUiState(isLoading = true))
+    val charactersDetailsUiState = _charactersDetailsUiState.asStateFlow()
 
-    private val _errorResponse = MutableLiveData<String>()
-    val errorResponse: LiveData<String> get() = _errorResponse
+    private val charactersList = mutableListOf<CharacterDetails>()
 
+    init {
+        getCharacters()
+    }
     fun getCharacters() {
         viewModelScope.launch {
-            val charactersResponse = charactersRepo.getCharacters()
-            Log.d("----->", charactersResponse.toString())
-            when (charactersResponse) {
-                is ApiResource.Success -> {
-                    _fetchCharacters.postValue(charactersResponse.value!!)
+            val characters = charactersRepo.getCharacters()
+            characters.collectLatest { charactersState ->
+                when (charactersState) {
+                    is ResponseState.Success -> {
+                        charactersList.apply {
+                            clear()
+                            addAll(charactersState.data.data)
+                        }
+                        _charactersDetailsUiState.update { it.copy(isLoading = false, characters = charactersState.data) }
+                    }
+
+                    is ResponseState.Error-> {
+                        _charactersDetailsUiState.update {
+                            it.copy(
+                                isLoading = false, errorMessage = charactersState.uiText
+                            )
+                        }
+                    }
                 }
-                is ApiResource.Error -> {
-                    _errorResponse.postValue(charactersResponse.errorBody.toString())
-                }
-                else -> {}
+
+            }
             }
         }
     }
 
-}
